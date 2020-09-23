6453ef84c1c9258f63075d75d90ae727347fb70d
Varsha G Bhat <55805353+Varsha-G-Bhat@users.noreply.github.com>
2020-09-23 20:10:48 +0530

    Add files via upload

---
+++ Number of pairs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def CountPairs(box, x):
+    """ Function to count the number of pairs
+    box - input list
+    x - number of Gloves in the given List"""
+    
+    j = 0
+    count = 0
+    box = sorted(box, reverse=False)
+    while  j < x-1 :
+        i = j
+        j = i + 1
+        if box[i] == box[j]:
+            j = j + 1
+            count = count + 1
+    return count
+
+
+
+if __name__ == "__main__":
+    N = int(input())
+    List = input().split()
+    print(CountPairs(List,N))
+
